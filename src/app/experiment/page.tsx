b4a395c71c2fa0eb543c52ce9ae8e583cdab3edb
hariwebchirpy <hari@webchirpy.com>
2024-12-25 21:15:00 +0530

    waiting for projects

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -3,8 +3,31 @@ import Container from '../_components/container'
 function page() {
   return (
     <Container>
-      <div>
-          Experiment or Projects
+      <div className='flex flex-col justify-start items-center pb-4  '>
+        <div className='text-2xl font-bold'>
+          Experiments
+        </div>
+        <div className='p-5'>
+          <h3> Balloons</h3>
+          <div>
+
+          </div>
+          <a href="#">pop-balloon</a>
+        </div>
+        <div className='p-5'>
+          <h3>Photo Gallery  </h3>
+          <div>
+
+          </div>
+          <a href="#">photo-gallery</a>
+        </div>
+        <div className='p-5'>
+          <h3>Pixel Draw  </h3>
+          <div>
+
+          </div>
+          <a href="#">pixel-draw</a>
+        </div>
       </div>
     </Container>
   )
